953021e127036c6b55ec0d03bf46fc803df2fc3b
Michael Schwed <schwed@pythonquants.com>
2018-03-21 11:15:40 +0100

    Add LICENSE.txt

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme():
 setup(
     name = 'fxcmpy',
     packages = ['fxcmpy'], # this must be the same as the name above
-    version = '1.1.10',
+    version = '1.1.11',
     description = 'A Python Wrapper Class for the RESTful API as provided by FXCM Forex Capital Markets Ltd.',
     long_description = readme(),
     author = 'The Python Quants GmbH',
@@ -22,6 +22,7 @@ setup(
     keywords = 'FXCM API Python Wrapper Finance Algo Trading',
     install_requires=['pandas', 'socketIO_client', 'configparser', 'requests'], 
     python_requires='>=3.4',
+    include_package_data = True,
     classifiers = ['Programming Language :: Python :: 3.4',
                    'Programming Language :: Python :: 3.5',
                    'Programming Language :: Python :: 3.6'],
